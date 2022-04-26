5e5af1cf71b22b842006e8eb5a152f4c853ebf86
Gabriel <gabriel1997.castro@gmail.com>
2022-04-26 12:03:27 -0300

    Basic img functions

---
+++ chapter2.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+img = cv2.imread('resources/test.png')
+kernel = np.ones((5, 5), np.uint8)
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(img, (7, 7), 0)
+imgCanny = cv2.Canny(img, 150, 200)
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
+
+imgGray = ResizeWithAspectRatio(imgGray, width=320)
+imgBlur = ResizeWithAspectRatio(imgBlur, width=320)
+imgCanny = ResizeWithAspectRatio(imgCanny, width=320)
+imgDialation = ResizeWithAspectRatio(imgDialation, width=320)
+imgEroded = ResizeWithAspectRatio(imgEroded, width=320)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image", imgCanny)
+cv2.imshow("Dialation Image", imgDialation)
+cv2.imshow("Erosion Image", imgEroded)
+
+
+cv2.waitKey(0)
